730f462e2873c67cdc8ecc5bceedb87e0c957bd6
B-Manish <batchumanish@gmail.com>
2024-11-26 00:49:51 +0530

    v1.0.5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 
-function Manish() {
-  return <div>Manish</div>;
+function Package({
+  itemHeight,
+  occupied,
+  itemsPerPage = Math.floor((window.innerHeight - occupied) / itemHeight) + 1,
+  data,
+  endReached,
+  ItemComponent,
+}) {
+  const [visibleStart, setVisibleStart] = useState(0);
+  const [visibleEnd, setVisibleEnd] = useState(0);
+  const containerRef = useRef(null);
+
+  const containerHeight = itemHeight * (itemsPerPage + 1);
+  const visibleItemCount = Math.ceil(containerHeight / itemHeight);
+
+  const handleScroll = (e) => {
+    const target = e.target;
+    const scrollBottom =
+      target.scrollHeight - target.scrollTop === target.clientHeight;
+
+    if (scrollBottom) {
+      endReached();
+    }
+
+    const scrollTop = containerRef.current.scrollTop;
+    const startIdx = Math.floor(scrollTop / itemHeight);
+    const endIdx = startIdx + visibleItemCount;
+
+    setVisibleStart(startIdx);
+    setVisibleEnd(endIdx);
+  };
+
+  useEffect(() => {
+    setVisibleEnd(visibleStart + visibleItemCount);
+  }, [visibleStart, visibleItemCount]);
+
+  return (
+    <div
+      style={{
+        height: `${window.innerHeight - occupied}px`,
+        overflowY: "scroll",
+        border: "1px solid red",
+      }}
+      onScroll={handleScroll}
+      ref={containerRef}
+    >
+      <div style={{ position: "relative" }}>
+        {data.slice(visibleStart, visibleEnd).map((item, index) => (
+          <div
+            key={index}
+            style={{
+              position: "absolute",
+              top: `${(index + visibleStart) * itemHeight}px`,
+              width: "100%",
+              height: `${itemHeight}px`,
+              overflow: "hidden",
+            }}
+          >
+            <ItemComponent item={item} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Manish;
+export default Package;
